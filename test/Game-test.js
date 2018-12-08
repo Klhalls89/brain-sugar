a3c883ea9b856bbedfa661b40a7270d7468812db
Whitney Burton <whitneyburton9@gmail.com>
2018-12-08 12:11:51 -0700

    Add first test (mock test) for Game class

--- test/Game-test.js
+++ test/Game-test.js
@@ -2,15 +2,19 @@ const chai = require('chai');
 const expect = chai.expect;
 const spies = require('chai-spies')
 chai.use(spies);
-const Game = require('../game.js');
-global.domUpdates = require('../domUpdates.js');
-global.round = require('../round.js');
-global.player = require('../player.js');
+const Game = require('../lib/game.js');
+global.domUpdates = require('../lib/domUpdates.js');
+global.round = require('../lib/rounds.js');
+global.player = require('../lib/player.js');
 
 
 describe('Game', function() {
-  it('should instatiate a new question object when ____ is called', function() {
-    game.makeQuestion();
-    expect(game.currentQuestion).to.equal()
-  })
-})
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  });
+
+  // it('should instatiate a new question object when ____ is called', function() {
+  //   game.makeQuestion();
+  //   expect(game.currentQuestion).to.equal()
+  // })
+});
