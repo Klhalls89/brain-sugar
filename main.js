1eb3e442e445bda5786feca632b19ad7c5620a8c
Kristen Hallstrom <klhalls89@gmail.com>
2018-12-10 13:48:34 -0700

    reorganize main js

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 
 
 $('.main-page').hide();
+$('.category').on('click', generatePopupCard)
+$('.js-submit-guess-btn').on('click', submitPlayerGuess)
+$('.js-start-button').on('click', startGame)
 
-$('.category').on('click', function(e){
+function generatePopupCard(e) {
   let category = $(e.target).siblings('.categories').attr('id').toLowerCase()
   let pointValue = parseInt($(event.target).text());
   let clue = new Clue()
   const foundClue = clue.findMatchingCard(category, pointValue);
-
-  
   domUpdates.createCard(foundClue);
-});
+};
 
-$('.js-start-button').on('click', function(e) {
+function startGame(e) {
   e.preventDefault();
+  let round = new Round();
   let playersArray = [
     new Player(document.querySelector('.js-p1-name-input').value).playerName, 
     new Player(document.querySelector('.js-p2-name-input').value).playerName, 
     new Player(document.querySelector('.js-p3-name-input').value).playerName
   ]
   let game = new Game(playersArray);
-  let round = new Round();
   domUpdates.updatePlayerNames(playersArray);
   $('.js-login-page').hide();
   $('.main-page').show();
-});
+};
 
-$('.js-submit-guess-btn').on('click', function() {
+function submitPlayerGuess() {
   let clue = new Clue();
-  clue.submitPlayerGuess(clue.categoryID);
-
-});
+  clue.comparePlayerGuess(clue.categoryID);
+};
