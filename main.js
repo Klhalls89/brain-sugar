1c7557bc1f6d7863f94c7267d7e9bcf3f3407c14
Whitney Burton <whitneyburton9@gmail.com>
2018-12-10 18:39:48 -0700

    Add functionality to track player scores within game class

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
-$('.main-page').hide();
-$('.category').on('click', generatePopupCard)
-$('.js-start-button').on('click', startGame)
-
 let playersArray = [
   new Player(document.querySelector('.js-p1-name-input').value), 
   new Player(document.querySelector('.js-p2-name-input').value), 
   new Player(document.querySelector('.js-p3-name-input').value)
 ];
 let game = new Game(playersArray);
+let currentClue;
 
+$('.main-page').hide();
+$('.category').on('click', generatePopupCard)
+$('.js-start-button').on('click', startGame)
+$('.js-submit-guess-btn').on('click', checkAnswer)
+
+function checkAnswer(){
+  game.comparePlayerGuess(currentClue);
+};
 
 function generatePopupCard(e) {
   let category = $(e.target).siblings('.categories').attr('id').toLowerCase();
   let pointValue = parseInt($(event.target).text());
   let foundClue = findMatchingClue(category, pointValue);
   domUpdates.createCard(foundClue);
-  let currentClue = new Clue(
+  currentClue = new Clue(
     foundClue.question, 
     foundClue.pointValue, 
     foundClue.answer, 
     foundClue.categoryID);
-  $('.js-submit-guess-btn').on('click', function(){
-    currentClue.comparePlayerGuess(game);
-    game.updatePlayerTurn();
-  })
 };
 
 function startGame(e) {
