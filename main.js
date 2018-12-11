559a15ef9531f7b957d07b6506b879674d1cfd2c
Whitney Burton <whitneyburton9@gmail.com>
2018-12-10 17:28:30 -0700

    Add comparePlayerGuess functionality for player scores

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
-
-
 $('.main-page').hide();
 $('.category').on('click', generatePopupCard)
 $('.js-start-button').on('click', startGame)
 
-function generatePopupCard(e) {
-  let category = $(e.target).siblings('.categories').attr('id').toLowerCase()
-  let pointValue = parseInt($(event.target).text());
-  let foundClue = findMatchingClue(category, pointValue)
-  domUpdates.createCard(foundClue);
-  let currentClue = new Clue(foundClue.question, foundClue.pointValue, foundClue.answer, foundClue.categoryID)
+let playersArray = [
+  new Player(document.querySelector('.js-p1-name-input').value), 
+  new Player(document.querySelector('.js-p2-name-input').value), 
+  new Player(document.querySelector('.js-p3-name-input').value)
+];
+let game = new Game(playersArray);
 
+
+function generatePopupCard(e) {
+  let category = $(e.target).siblings('.categories').attr('id').toLowerCase();
+  let pointValue = parseInt($(event.target).text());
+  let foundClue = findMatchingClue(category, pointValue);
+  domUpdates.createCard(foundClue);
+  let currentClue = new Clue(
+    foundClue.question, 
+    foundClue.pointValue, 
+    foundClue.answer, 
+    foundClue.categoryID);
   $('.js-submit-guess-btn').on('click', function(){
-    currentClue.comparePlayerGuess();
-    // update player turn 
+    currentClue.comparePlayerGuess(game);
+    game.updatePlayerTurn();
   })
 };
 
 function startGame(e) {
   e.preventDefault();
   let round = new Round();
-  let playersArray = [
-    new Player(document.querySelector('.js-p1-name-input').value), 
-    new Player(document.querySelector('.js-p2-name-input').value), 
-    new Player(document.querySelector('.js-p3-name-input').value)
-  ]
-  playersArray[0].isTurn = true;
-  console.log(playersArray)
-
-  let game = new Game(playersArray);
+  // playersArray[0].isTurn = true;
   domUpdates.updatePlayerNames(playersArray);
   $('.js-login-page').hide();
   $('.main-page').show();
