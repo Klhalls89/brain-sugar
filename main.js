e1478d2e29256eb8c9727488c8a8050814b51063
Whitney Burton <whitneyburton9@gmail.com>
2018-12-13 16:32:59 -0700

    tease out oop

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-let players;
 let game;
 let currentClue;
-let round;
 
 $('.main-page').hide();
 $('.category').on('click', generatePopupCard);
 $('.js-start-button').on('click', startGame);
 $('.js-submit-guess-btn').on('click', submitAnswer);
-$('.js-submit-wager-btn').on('click', updatePoints);
+$('.js-submit-wager-btn').on('click', updateWagerPoints);
 
 function submitAnswer() {
-  game.updateDisplayScore(currentClue);
-  round.removeClue(currentClue);
+  let result = currentClue.validateAnswer();
+  game.players[game.currentPlayer].updateScore(result, currentClue.pointValue);
+  domUpdates.displayScore(game.players);
   game.updatePlayerTurn();
+  game.round.removeClue(currentClue);
+  domUpdates.hideCard();
 }
 
 function generatePopupCard(e) {
@@ -31,7 +32,7 @@ function generatePopupCard(e) {
   domUpdates.disableSquare(e.target);
 }
 
-function updatePoints() {
+function updateWagerPoints() {
   let dailyDouble = new DailyDouble();
   let userWager = $('.js-wager-input').val();
   if (dailyDouble.validateWager(userWager) === 'valid wager') {
@@ -42,18 +43,17 @@ function updatePoints() {
 
 function startGame(e) {
   e.preventDefault();
-  round = new Round();
-  round.createCategories();
-  round.assignDailyDouble();
-  players = domUpdates.getPlayerNames();
-  game = new Game(players);
+  let players = domUpdates.getPlayerNames();
   domUpdates.updatePlayerNames(players);
+  game = new Game(players);
+  game.round.createCategories();
+  game.round.assignDailyDouble();
   $('.js-login-page').hide();
   $('.main-page').show();
 }
 
 function findMatchingClue(category, pointValue) {
-  return round.clues.find((clue) => {
+  return game.round.clues.find((clue) => {
     if (clue.categoryId == category && clue.pointValue == pointValue) {
       return clue;
     }
